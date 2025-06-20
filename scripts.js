bffc6bf615b5363be51c77bde515fba75974b959
Lingranger <abuzogodofredo4@gmail.com>
2025-06-20 09:43:29 +0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("logout-button").addEventListener("click", function (e) {
-  e.preventDefault();
-  localStorage.removeItem("userLoggedIn");
-  sessionStorage.removeItem("userSessionLogged");
-  window.location.href = "advertisement.html";
-});
